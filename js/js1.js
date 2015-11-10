ad22ea408aea4a37b16cc0300c478d3f2a6a1ad4
gorbachevsa <gorbachrv.sa91@gmail.com>
2015-11-11 02:37:29 +0300

    add game chat buttom

---
+++ js/js1.js
@@ -0,0 +1,29 @@
+﻿function monday(){
+	var day=new Date();
+	var weekday=new Array(7);
+	weekday[0]="Воскресенье";
+	weekday[1]="Понедельник";
+	weekday[2]="Вторник";
+	weekday[3]="Среда";
+	weekday[4]="Четверг";
+	weekday[5]="Пятница";
+	weekday[6]="Суббота";
+	alert("Сегодня " + weekday[day.getDay()]);
+	}
+	function data(){
+	var day=new Date();
+	var month=new Array(11);
+	month[0]="января";
+	month[1]="февраля";
+	month[2]="марта";
+	month[3]="апреля";
+	month[4]="мая";
+	month[5]="июня";
+	month[6]="июля";
+	month[7]="августа";
+	month[8]="сентября";
+	month[9]="октября";
+	month[10]="ноября";
+	month[11]="декабря";
+	alert("Сегодня " + day.getDay()+ " " + month[day.getMonth()]+" "+day.getFullYear()+" года, "+"на часах: "+ day.getHours()+":"+day.getMinutes());
+	}
---
+++ js/knb.js
@@ -0,0 +1,69 @@
+﻿document.getElementById("w").onclick = function(){
+var z = document.getElementsByName('q'),s=0;
+var iq=Math.random();
+var reshenie;
+for (var i = 0; i < z.length; i++)
+	{	
+		if  (document.getElementsByName('q')[i].checked == true) {
+			reshenie=i;
+		} 
+		if  (document.getElementsByName('q')[i].checked == false) {
+			s = s+1;
+		} 
+		if(s==3){
+			alert("Выбери свое оружие");
+		}
+	}
+	if(reshenie==0){
+		reshenie="Камень";
+	}
+	else if(reshenie==1){
+		reshenie="Ножницы"
+	}
+	else{
+		reshenie="Бумага"
+	}
+	alert("Выбор: " + reshenie);
+  
+	if (iq < 0.34) {
+      iq = "Камень";
+    }
+    else if (iq <= 0.67) {
+      iq = "Ножницы";
+    }
+    else {
+      iq = "Бумага";
+    }
+    alert("Компьютер: " + iq);
+var choice1 = reshenie;
+var choice2 = iq;
+	
+    
+      if (choice1 === choice2) {
+          alert("Никто не победил!");
+      }
+      else if (choice1 === "Камень") {
+        if (choice2 === "Ножницы") {
+          alert("Камень победил!");
+        }
+        else {
+          alert("Бумага победила!");
+        }
+      }
+      else if (choice1 === "Бумага") {
+        if (choice2 === "Камень") {
+          alert("Бумага победила!");
+        }
+        else {
+          alert("Ножницы победили!");
+        }
+      }
+      else if (choice1 === "Ножницы") {
+        if (choice2 === "Бумага") {
+          alert("Ножницы победили!");
+        }
+        else {
+          alert("Камень победил!");
+		}
+      }
+    }
